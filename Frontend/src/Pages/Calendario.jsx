e121f50b6016039285809f1f2925681bb8045276
Christopher Iza <cdiza5@espe.edu.ec>
2024-12-28 12:31:03 -0500

    pagina fondo blanco calendario

--- Frontend/src/Pages/Calendario.jsx
+++ Frontend/src/Pages/Calendario.jsx
@@ -1,7 +1,16 @@
 import Calendar from "../Components/Calendar";
 
 function Calendario() {
-  return <Calendar />;
+  return (
+    <>
+      <div className="container mx-auto p-4">
+        <div className="mb-4">
+          <h1 className="text-2xl font-bold">Lista Profesores</h1>
+        </div>
+        <Calendar />
+      </div>
+    </>
+  );
 }
 
 export default Calendario;
